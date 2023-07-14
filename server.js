9680cef6ca74fb0b89e28d3fdff911176649a051
rutu-pandav <ruturajpandav@outlook.com>
2023-07-14 22:34:24 +0530

    code for changepassword , deleteaccount

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ const app = express();
 const multer = require('multer');
 const fs = require('fs');
 const AWS = require('aws-sdk');
+const Pool = require('pg').Pool
 const http = require('http').createServer(app);
+const jwt = require('jsonwebtoken');
 
 
 app.use(express.json())
 app.use(cors())
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'realtimechat',
+  password: '996653255',
+  port: 5432,
+})
 
 
 const storage = multer.diskStorage({
@@ -40,10 +50,8 @@ const socketIO = require("socket.io")(http, {
 
 
 socketIO.on('connection', (socket) => {
-
   socket.on('joinRoom', (data) => {
     console.log("joinRoom event :", data)
-
     socket.broadcast.emit('userJoined', `${data.name} joined`);
   });
   socket.on('message', (data) => {
@@ -59,12 +67,65 @@ socketIO.on('connection', (socket) => {
 
 
 
-app.post('/register', upload.single('file'), (req, res) => { 
-  const email = req.body.email;
-  const lastname = req.body.lastname;
-  const firstname = req.body.firstname;
-  const status = req.body.status;
-  const password = req.body.password;
+app.delete("/delete-account", (req, res) => {
+  try {
+
+
+    const query = 'DELETE FROM users WHERE user_id = $1';
+    const values = [req.body.user_id]
+    pool.query(query, values, (error, results) => {
+      if (error) {
+        throw error
+      }
+    })
+    res.json({
+      msg: "OK"
+    })
+
+  } catch (error) {
+    res.json({
+      msg: err.message
+    })
+  }
+})
+
+app.post("/change-password", (req, res) => {
+  try {
+    const query = `UPDATE users SET password = $1 WHERE user_id = $2`
+    console.log(req.body.password, req.body.user_id)
+    const values = [req.body.password, req.body.user_id]
+    pool.query(query, values, (error, results) => {
+      if (error) {
+        throw error
+      }
+      console.log("reslt :", results.rows)
+    })
+    res.json({
+      msg: "OK"
+    })
+  }
+  catch (err) {
+    console.log("ERROR ::::")
+    res.json({
+      msg: err.message
+    })
+  }
+})
+app.get("/users", (req, res) => {
+  const query = 'SELECT * FROM users';
+
+  pool.query(query, (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+
+})
+
+
+app.post('/register', upload.single('file'), (req, res) => {
+
   const params = {
     Bucket: 'user-dps',
     Key: req.file.originalname,
@@ -77,23 +138,24 @@ app.post('/register', upload.single('file'), (req, res) => {
       res.status(500).send('Error uploading image to S3');
     } else {
       console.log(`Image uploaded to S3: ${data.Location}`);
-      
+
+      const query = 'INSERT INTO users(firstname , lastname , email , password , status , profile , key ) VALUES($1 , $2 , $3 , $4 , $5 , $6 , $7) RETURNING *';
+      const values = [req.body.firstname, req.body.lastname, req.body.email, req.body.password, "sample-status-for-now", data.Location, req.file.originalname]
+      pool.query(query, values, (error, results) => {
+        if (error) {
+          throw error
+        }
+        let email = req.body.email
+        let expiresIn = "12h"
+        const token = jwt.sign({ email }, 'your_secret_key_here', { expiresIn });
+
+      })
       fs.unlinkSync(`uploads/${req.file.originalname}`)
+
       res.status(200).send('Image uploaded successfully');
     }
   });
-  // console.log("/register")
-  // const email = req.body.email;
-  // const lastname = req.body.lastname;
-  // const firstname = req.body.firstname;
-  // const status = req.body.status;
-  // const password = req.body.password;
-  // res.json({
-  //   msg:"OK" ,
-  //   data : {
-  //     email , lastname , firstname , status , password
-  //   }
-  // })
+
 })
 
 
